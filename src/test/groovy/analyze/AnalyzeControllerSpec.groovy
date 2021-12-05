85d95f7de5ac0151822b0137e5119d45843d216d
JamesChang <james89926282@gmail.com>
2021-12-06 00:13:05 +0800

    feat: 初次新增

---
+++ src/test/groovy/analyze/AnalyzeControllerSpec.groovy
@@ -0,0 +1,18 @@
+package analyze
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class AnalyzeControllerSpec extends Specification implements ControllerUnitTest<AnalyzeController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
